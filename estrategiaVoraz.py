01f07aa13034db92f1b92bd524fdd41772fb7b20
Sergio Escudero Tabares <95147293+SergioSET@users.noreply.github.com>
2023-04-20 11:10:33 -0500

    Se agregó la estrategia voraz

---
+++ estrategiaVoraz.py
@@ -0,0 +1,63 @@
+import sys
+import os
+import time
+from typing import Tuple
+
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ''))
+sys.path.insert(0, root_dir)
+
+from main import *
+
+if __name__ == '__main__':
+    main()
+
+def estrategiaVoraz(A: int, B: int, n: int, ofertas: tuple):
+    asignacion = tuple
+    asignaciones = tuple
+    def valor(asignaciones: tuple, ofertas: tuple):
+        val = 0
+        for i, oferta in enumerate(ofertas):
+            val += (oferta[0] * asignaciones[i])
+            # print("asig:" , asignaciones[i], " valor: ", val)
+        return val
+
+    def calculo(A,B,n,ofertas):
+        asignaciones = []
+        for oferta in ofertas:
+            accion = 0
+            if (oferta[2] <= A):
+                A -= oferta[2]
+                accion = oferta[2]
+                while (accion < oferta[1] and A):
+                    A -= 1
+                    accion += 1
+            asignaciones.append(accion)
+        return asignaciones, valor(asignaciones, ofertas)
+
+            # if (A >= oferta[1]):
+            #     A -= oferta[1]
+            #     asignaciones.append(oferta[1])
+            #     print(A)
+            #     print(asignaciones)
+            # elif (oferta[1]>=A<=oferta[2]):
+            #     acciones = 0
+            #     while (A >= oferta[2]):
+            #         A -= 1
+            #         acciones += 1
+            #     asignaciones.append(acciones)
+            # else:
+            #     asignaciones.append(0)
+
+    if n == 0:
+        return [A], B * A
+    else:
+        return calculo(A,B,n,ofertas)
+
+def accionesVoraz(A: int, B: int, n: int, ofertas: tuple):
+    print("Se iniciará el algoritmo voraz")
+    with open("outputVoraz.txt", 'w') as write_file:
+        solucion, valorSolucion = estrategiaVoraz(A, B, n, ofertas)
+        write_file.write("{0}\n".format(valorSolucion))
+        for asig in solucion:
+            write_file.write("{0}\n".format(asig))
+    print("Se ha finalizado el algoritmo voraz, revise el archivo outputVoraz.txt para ver los resultados")
