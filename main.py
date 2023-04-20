e93f280e104f9b5b5d5da9eaf34227b98ad4212a
Sergio Escudero Tabares <95147293+SergioSET@users.noreply.github.com>
2023-04-20 15:23:18 +0000

    Main funcional

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 def main():
-    input = open("input.txt", 'r')
+    inputTxt = open("input.txt", 'r')
      
-    A = int(input.readline())
-    B = int(input.readline())
-    n = int(input.readline())
+    A = int(inputTxt.readline())
+    B = int(inputTxt.readline())
+    n = int(inputTxt.readline())
     ofertas = ()
 
-    # Se crea una tupla de tuplas leyendo el archivo input.txt
+    # Se crea una tupla de tuplas leyendo el archivo inputTxt.txt
     for i in range(n + 1):
-        p, max, min = input.readline().split(sep=', ')
+        p, max, min = inputTxt.readline().split(sep=', ')
         tupla = (int(p), int(max), int(min))
         ofertas += tuple([tupla])
 
@@ -17,11 +17,8 @@ def main():
     print("Número de ofertantes ", n)
     print("Lista de ofertantes ", ofertas, "\n")
 
-    # algoritmo = input("¿Qué tipo de algoritmo desea utilizar?")
-    # # "\n", "(1) Estrategia de Fuerza Bruta",
-    # # "\n", "(2) Estrategia Voraz",
-    # # "\n", "(3) Estrategia de Programación dinamica")
-    # print(algoritmo)
+    algoritmo = input("¿Qué tipo de algoritmo desea utilizar?\n(1) Estrategia de Fuerza Bruta\n(2) Estrategia Voraz\n(3) Estrategia de Programación dinamica\n")
+    
 
 if __name__ == '__main__':    
     main()
